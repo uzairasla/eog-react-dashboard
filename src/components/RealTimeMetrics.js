4eed103010bfede8e0955eb2a39d32eebe384535
uzairaslam456 <uaslam@mjhlifesciences.com>
2021-05-07 09:24:38 -0500

    adjusted note styles

--- src/components/RealTimeMetrics.js
+++ src/components/RealTimeMetrics.js
@@ -6,7 +6,7 @@ const MetricsListed = () => {
   const selected_metrics = useSelector(state => state.measurements.selectedMetrics)
 
   if (selected_metrics.length === 0) {
-    return <h3 style={{margin: '0 auto'}}>Please select metrics to view</h3>
+    return <p style={{textAlign: 'center'}}>Please select metrics to view</p>
   }
   return (
     <div>
