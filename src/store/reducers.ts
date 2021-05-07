d32b6fc2513d0de2fc242f8257e8dfb2930687b7
uzairaslam456 <uaslam@mjhlifesciences.com>
2021-05-06 23:24:40 -0500

    added new reducer to combine reducer object

--- src/store/reducers.ts
+++ src/store/reducers.ts
@@ -1,5 +1,6 @@
 import { reducer as weatherReducer } from '../Features/Weather/reducer';
-
+import {measurementReducer} from '../Features/Measurements/measurementReducer'
 export default {
   weather: weatherReducer,
+  measurements: measurementReducer
 };
